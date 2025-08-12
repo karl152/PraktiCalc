10dddaeb0ac89bf66dd1c3a0d4b921dec4dd6ef8
karl152 <karl.david.wes@icloud.com>
2025-08-12 18:34:32 +0200

    update simple info dialog to show GPLv3 insted of GPLv3+

--- prakticalc.py
+++ prakticalc.py
@@ -56,7 +56,7 @@ HexadezimalZahl = 0
 # Funktionen definieren
 def info() :
     if CustomMsgBox == 0 :
-        messagebox.showinfo("PraktiCalc 1.3 (in development), licensed under GPLv3", "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.")
+        messagebox.showinfo("PraktiCalc 1.3 (in development), licensed under GPLv3", "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.")
     if CustomMsgBox == 1 :
         CustomInfo()
 def eins() :
