59fa4d47758dee5c0682a9b552e369958a3420a1
karl152 <karl.david.wes@icloud.com>
2025-11-02 22:00:54 +0100

    add license banner to appimage_build.sh

--- appimage_build.sh
+++ appimage_build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# PraktiCalc © 2024-2025 Karl "karl152"
+# Licensed under the GNU General Public License v3.0.
+# See https://www.gnu.org/licenses/gpl-3.0.txt for details.
+
 echo "Cleaning"
 rm linux-pkg-builds/AppImage/PraktiCalc-x86_64.AppImage
 
