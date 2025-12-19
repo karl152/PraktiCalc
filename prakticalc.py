5e5917f3406d0187c573f828edf5e8d800fb7ae2
Karl <karl.david.wes@icloud.com>
2025-12-19 11:05:59 +0100

    Update version number for test release

--- prakticalc.py
+++ prakticalc.py
@@ -39,7 +39,7 @@ import time
 # variables
 CLIHelp = "--help" in sys.argv
 CLIVersion = "--version" in sys.argv
-PraktiCalcVersion = "1.4"
+PraktiCalcVersion = "1.4test1"
 BypassWindowsDPIFix = "--nodpiawareness" in sys.argv
 allowWindowsShutdownDialog = "--allowShutdownDialog" in sys.argv
 if platform.system() == "Windows":
