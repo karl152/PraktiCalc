9f8b78ad1947c2fa747f869325f7c14772a6144b
Karl <karl.david.wes@icloud.com>
2026-01-08 17:21:39 +0100

    add dynamic architecture support for AppImage build script

--- appimage_build.sh
+++ appimage_build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# PraktiCalc © 2024-2025 Karl "karl152"
+# PraktiCalc © 2024-2026 Karl "karl152"
 # Licensed under the GNU General Public License v3.0.
 # See https://www.gnu.org/licenses/gpl-3.0.txt for details.
 
 echo "Cleaning"
-rm linux-pkg-builds/AppImage/PraktiCalc-x86_64.AppImage
+rm linux-pkg-builds/AppImage/PraktiCalc-$(uname -m).AppImage
 
 mkdir -p linux-pkg-builds/AppImage/com.github.karl152.prakticalc.AppDir/usr/bin
 
@@ -44,20 +44,20 @@ chmod +x ./linux-pkg-builds/AppImage/com.github.karl152.prakticalc.AppDir/AppRun
 chmod +x ./linux-pkg-builds/AppImage/com.github.karl152.prakticalc.AppDir/usr/bin/prakticalc
 echo ""
 echo "------------------------------------------"
-echo "The script will now download appimagetool from https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage. If you don't want that, press CTRL+C now."
+echo "The script will now download appimagetool from https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$(uname -m).AppImage. If you don't want that, press CTRL+C now."
 echo "------------------------------------------"
 sleep 10
 echo "Downloading appimagetool"
-wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
-chmod +x appimagetool-x86_64.AppImage
-mv ./appimagetool-x86_64.AppImage ./linux-pkg-builds/AppImage/
+wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$(uname -m).AppImage
+chmod +x appimagetool-$(uname -m).AppImage
+mv ./appimagetool-$(uname -m).AppImage ./linux-pkg-builds/AppImage/
 
 echo "Building AppImage"
 cd ./linux-pkg-builds/AppImage
-./appimagetool-x86_64.AppImage com.github.karl152.prakticalc.AppDir
+./appimagetool-$(uname -m).AppImage com.github.karl152.prakticalc.AppDir
 
 echo "Cleaning"
-rm appimagetool-x86_64.AppImage
+rm appimagetool-$(uname -m).AppImage
 cd ../..
 rm -rf ./linux-pkg-builds/AppImage/com.github.karl152.prakticalc.AppDir/usr/bin
 rm -rf dist/
